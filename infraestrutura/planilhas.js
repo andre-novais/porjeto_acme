f84d2134335cf551d610399f0a3a6737d6f3a740
andre <andrenovais@usp.br>
2020-03-02 09:16:03 -0300

    ajuste_na_planilha

--- infraestrutura/planilhas.js
+++ infraestrutura/planilhas.js
@@ -4,6 +4,13 @@ class Planilha{
     }
     cria_planilha(){
         const sql = 'CREATE TABLE T_financeiro IF NOT EXISTS (id PRIMARY KEY AUTO INCREMENT, DS_tipo varchar(50) not NULL, VL_valor integer not null, occured_at epoch default extract(epoch from now()))'
+        this.conexao.query(sql, erro => {
+            if(erro) {
+                console.log(erro)
+            } else {
+                console.log('Tabela Atendimentos criada com sucesso')
+            }
+        })
     }
 }
 
