30bbc151e3ef46e5fdd53df68621e1791d1fa9f0
andre <andrenovais@usp.br>
2020-03-05 22:48:27 -0300

    regra de negocios na api

--- infraestrutura/coneccao.js
+++ infraestrutura/coneccao.js
@@ -8,11 +8,11 @@ const coneccao = new Client({
 })
 
 var tabelas = [
-    'CREATE TABLE IF NOT EXISTS T_financeiro (id SERIAL PRIMARY KEY, DS_tipificacao varchar(50) not NULL, VL_valor integer not null, occured_at int default extract(epoch from now()))',
-    'CREATE TABLE IF NOT EXISTS T_crm (id SERIAL PRIMARY KEY, ID_cliente integer not NULL, DS_evento varchar(20), occured_at int default extract(epoch from now()))',
-    'CREATE TABLE IF NOT EXISTS T_loja_online (id SERIAL PRIMARY KEY , ID_compra integer NOT NULL, ID_cliente int not null, ARR_produtos varchar(30)[] not NULL, BO_checkout boolean default FALSE, JS_dados_financeiros JSON default NULL, occured_at int default extract(epoch from now()));',
-    'CREATE TABLE IF NOT EXISTS T_fornecedores (id SERIAL PRIMARY KEY, CD_fornecedor integer NOT NULL, DS_evento varchar(20) NOT NULL, ID_produto integer NOT NULL, VL_quantidade integer not null, VL_transacao integer NOT NULL, occured_at int default extract(epoch from now()))',
-    'CREATE TABLE IF NOT EXISTS T_rh (id SERIAL PRIMARY KEY, CD_funcionario integer not NULL, DS_evento varchar(30) NOT NULL, DS_cargo varchar(30) NOT NULL,occured_at int default extract(epoch from now()))'
+    'CREATE TABLE IF NOT EXISTS T_financeiro (id SERIAL PRIMARY KEY, DS_tipificacao varchar(50) not NULL, VL_valor int not null, occured_at int default extract(epoch from now()))',
+    'CREATE TABLE IF NOT EXISTS T_crm (id SERIAL PRIMARY KEY, ID_cliente int not NULL, DS_evento varchar(20), occured_at int default extract(epoch from now()))',
+    'CREATE TABLE IF NOT EXISTS T_loja_online (id SERIAL PRIMARY KEY , ID_compra int NOT NULL, ID_cliente int not null, ARR_produtos varchar(30)[] not NULL, BO_checkout boolean default FALSE, JS_dados_financeiros JSON default NULL, occured_at int default extract(epoch from now()));',
+    'CREATE TABLE IF NOT EXISTS T_fornecedores (id SERIAL PRIMARY KEY, CD_fornecedor int NOT NULL, DS_evento varchar(20) NOT NULL, ID_produto int NOT NULL, VL_quantidade int not null, VL_transacao int NOT NULL, occured_at int default extract(epoch from now()))',
+    'CREATE TABLE IF NOT EXISTS T_rh (id SERIAL PRIMARY KEY, CD_funcionario int not NULL, DS_evento varchar(30) NOT NULL, DS_cargo varchar(30) NOT NULL,occured_at int default extract(epoch from now()))'
 ]
 
 tabelas.forEach(tabela => {
