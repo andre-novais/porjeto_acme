f63575f35fb23e58750edc9afff04b4d959a803f
andre <andrenovais@usp.br>
2020-03-06 10:42:55 -0300

    last fixes

--- controler/financeiro.js
+++ controler/financeiro.js
@@ -7,13 +7,13 @@ module.exports = app => {
     app.get('/financeiro/:id', (req,res) => {
         const id = parseInt(req.params.id)
         resposta = instancia_planilha.pega_por_id(id, resposta)
-        console.log(resposta)
         res.send(resposta)
     })
     app.post('/financeiro', (req, res) => {
         console.log('Evento enviado')
         console.log(req.body)
         resposta = instancia_planilha.insere(req.body, resposta)
+        console.log(resposta)
         res.send(resposta)
     })
     app.put('/financeiro/:id', (req,res)=>{
