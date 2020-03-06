eab3ed001717bafc6b575a4fb8068b1f790db1c2
andre <andrenovais@usp.br>
2020-03-05 23:27:02 -0300

    regra de negocios na api

--- models/loja_online.js
+++ models/loja_online.js
@@ -3,7 +3,7 @@ const Plan_base = require("./plan_base.js")
 class Loja_online extends Plan_base {
     constructor(conn){
         super(conn);
-        this.items ={"ID_compra": "number", "ID_cliente": "number","ARR_produtos": "object", "ID_produto":"number","BO_checkout": "boolean", "JS_dados_financeiros": "object"}
+        this.items ={"ID_compra": "number", "ID_cliente": "number","ARR_produtos": "object","BO_checkout": "boolean", "JS_dados_financeiros": "object"}
         this.table_name = "T_loja_online"
     }
     insere(json, res){
