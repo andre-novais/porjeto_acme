480cb6af64e2f3a9fd33bf7ae5adfcbdc4034d85
andre <andrenovais@usp.br>
2020-03-05 23:19:29 -0300

    regra de negocios na api

---
+++ models/loja_online.js
@@ -0,0 +1,23 @@
+const Plan_base = require("./plan_base.js")
+
+class Loja_online extends Plan_base {
+    constructor(conn){
+        super(conn);
+        this.items ={"ID_compra": "number", "ID_cliente": "number","ARR_produtos": "object", "ID_produto":"number","BO_checkout": "boolean", "JS_dados_financeiros": "object"}
+        this.table_name = "T_loja_online"
+    }
+    insere(json, res){
+        super.insere(json, res)
+    }
+    pega_por_id(id, res){
+        super.pega_por_id(id, res)
+    }
+    altera(id,json,res){
+        super.altera(id,json,res)
+    }
+    deleta(id,res){
+        super.deleta(id,res)
+    }
+}
+
+module.exports = Loja_online
