7be93681c536807f5c1f41fa158b4174d424f504
andre <andrenovais@usp.br>
2020-03-07 09:50:50 -0300

    last fixes

--- controler/financeiro.js
+++ controler/financeiro.js
@@ -11,7 +11,7 @@ module.exports = app => {
         }
     })
     app.post('/financeiro', (req, res) => {
-        instancia_planilha.insere(req.body);
+        instancia_planilha.insere(req.body, res);
         if (!(res.headerSent)){
             res.status(200).send(req.body)
         }
