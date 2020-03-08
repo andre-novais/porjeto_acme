df81aa57eb51628d9f6fcec7b573d7fea1960805
andre <andrenovais@usp.br>
2020-03-08 13:30:06 -0300

    millisiconds

--- models/plan_base.js
+++ models/plan_base.js
@@ -23,7 +23,7 @@ class Plan_base{
         if (this._valida(json)===true){
             const converte = (str) => (typeof(str) == "string")? "'"+str+"'":(typeof(str)=="object")?"'"+JSON.stringify(str)+"'":str;
             const valores = Object.values(json).map(converte)
-            let sql = `insert into ${this.table_name} (${Object.keys(json)}) Values (${valores.join(',')}) RETURNING id`
+            let sql = `insert into ${this.table_name} (${Object.keys(json)} , occured_at) Values (${valores.join(',')}, ${Date.now() - 15778458000}) RETURNING id`
             
             this.conn.query(sql, (erro, resultado) => {
                 if(erro) {
