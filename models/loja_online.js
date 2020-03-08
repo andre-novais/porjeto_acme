7b305cb0b15fa5bcba6dcc8510f9a9e5e877f64f
andre <andrenovais@usp.br>
2020-03-08 17:07:01 -0300

    millisiconds

--- models/loja_online.js
+++ models/loja_online.js
@@ -25,8 +25,7 @@ class Loja_online extends Plan_base {
             var instancia_crm = new Crm(conn)
             var instancia_financeiro = new Financeiro(conn)
             instancia_crm.insere({"ID_cliente":json["ID_cliente"],"DS_evento":"cliente_ativado"},res)
-            instancia_financeiro.insere({"VL_tipificacao":"custo","VL_valor":this._soma_items(json["JS_produtos"])}, res)
-        
+            instancia_financeiro.insere({"DS_tipificacao":"custo","VL_valor":this._soma_items(json["JS_produtos"])}, res)
             const produto_sem_estoque = "19"
             if(Object.keys(json["JS_produtos"]).includes(produto_sem_estoque)){
                 var instancia_fornecedores = new Fornecedores(conn) 
